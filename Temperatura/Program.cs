fdce3d892510a83eb624eb9ca83a3853efe6aa7a
Willian Marioti <willian.marioti@gmail.com>
2023-03-05 13:23:53 -0300

    Add project files.

---
+++ Temperatura/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double temp, fa;
+
+        Console.WriteLine("Informe a temperatura: ");
+        temp = double.Parse(Console.ReadLine());
+
+        fa = (temp * 1.8) + 32;
+
+        Console.WriteLine("A temperatura em fahrenheit é: {0}",fa);
+    }
+}
